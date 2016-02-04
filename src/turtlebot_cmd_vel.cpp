3ae4aed0e8bbeae6b4552c5e28b9c96bc7059fb0
Chris <cwg930@gmail.com>
2016-02-03 21:13:31 -0500

    added cmd_vel based movement code

---
+++ src/turtlebot_cmd_vel.cpp
@@ -0,0 +1,57 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Vector3.h>
+
+class TurtlebotCmdVel
+{
+  ros::NodeHandle nh_;
+  // geometry_msgs::Twist moveCommand;
+  sensor_msgs::LaserScan scanData;
+  ros::Subscriber vector_sub;
+  ros::Subscriber laser_sub;
+  ros::Publisher move_pub;
+
+public:
+  TurtlebotCmdVel()
+  {
+    vector_sub = nh_.subscribe("/targetPos", 1, &TurtlebotCmdVel::vectorCb, this);
+    laser_sub = nh_.subscribe("/scan", 1, &TurtlebotCmdVel::laserCb, this);
+    move_pub = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  }
+  
+  void vectorCb(const geometry_msgs::Vector3& msg)
+  {
+    geometry_msgs::Twist moveCommand;
+    moveCommand.linear.x = 0.2;
+    move_pub.publish(moveCommand); 
+    ROS_INFO("MOVING 1m");
+  }
+
+  void laserCb(const sensor_msgs::LaserScan& msg)
+  {
+    int numRanges = (int)(msg.angle_max - msg.angle_min) / msg.angle_increment;
+    int minRange = 0.75;
+    geometry_msgs::Twist moveCommand;
+    for(int i = 0; i < numRanges; i++)
+      {
+	if(msg.ranges[i] < minRange){
+	  moveCommand.linear.x = 0;
+	  moveCommand.linear.y = 0;
+	  moveCommand.linear.z = 0;
+	  moveCommand.angular.x = 0;
+	  moveCommand.angular.y = 0;
+	  moveCommand.angular.z = 0;
+	}
+      }
+  }
+};
+
+  int main(int argc, char** argv)
+  {
+    ros::init(argc, argv, "turtlebot_cmd_vel");
+    TurtlebotCmdVel tbc;
+    ros::spin();
+
+    return 0;
+  }
